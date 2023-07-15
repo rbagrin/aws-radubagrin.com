c589d3783f8d918c97c63711ae29b10de610a12a
Radu Bagrin <bagrin.radu@gmail.com>
2023-07-15 17:45:27 +0100

    Make api call from FE

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsradubagrincom": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "awsradubagrincom34cfc6ae": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "GetResume": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,26 @@ import {
   Card,
 } from "@aws-amplify/ui-react";
 
+import Amplify, {API, Auth} from 'aws-amplify'
+import { useState } from "react";
+
 function App({ signOut }: {signOut: () => void}) {
+  const [response, setResponse] = useState<Record<string, any> | null>(null);
+
+  async function callApi() {
+    const user = await Auth.currentAuthenticatedUser()
+    const token = user.signInUserSession.idToken.jwtToken
+
+    const requestData = {
+        headers: {
+            Authorization: token
+        }
+    }
+    const data = await API.get('awsradubagrincom', '/resume', requestData)
+    console.log("data: ", data)
+    setResponse(data);
+  }
+
   return (
     <View className="App">
       <Card>
@@ -17,6 +36,10 @@ function App({ signOut }: {signOut: () => void}) {
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
+
+      <Button onClick={callApi}>Call API</Button>
+
+      {response && <Card>{JSON.stringify(response, null, 2)}</Card>}
     </View>
   );
 }
